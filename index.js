9d1e73343a7782bb4ae667ab8f81f768a7abfd0d
Jennifer Gaillard <jengaillard1@gmail.com>
2019-02-02 05:20:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file.
+students.map( s => !!s.permissionSlip && s.isVegetarian) // Find vegetarians w/ permission slips on file
+  .map( s => s.preferredMeal ) // Extract the meal name
+  .filter( s => s.match(/curry/i) // Find those who like curry, case-insensitive
+  .length);
+ 
+// => "How many students going on the field trip should we order curry for?"
